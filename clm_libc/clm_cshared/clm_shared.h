7fea3d989cb7655d74108eccc561929e9fd87cf8
lunarium <claymorealt@op.pl>
2023-09-27 20:44:57 +0200

    Add shared code module

---
+++ clm_libc/clm_cshared/clm_shared.h
@@ -0,0 +1,35 @@
+#ifndef CLM_SHARED_GUARD_
+#define CLM_SHARED_GUARD_
+
+#include <stddef.h>
+#include <stdint.h>
+
+
+/******************************************************************************
+ * TYPE ALIASES
+ *****************************************************************************/
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef u8 byte;
+
+typedef uintptr_t usize;
+typedef intptr_t isize;
+
+typedef uintptr_t uptr;
+typedef intptr_t iptr;
+typedef ptrdiff_t ptrdiff;
+
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+
+#endif /* CLM_SHARED_GUARD_ */
