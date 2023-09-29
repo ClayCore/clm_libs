f61bd228c7d38c917416d9f78d3d3b04fa824469
lunarium <claymorealt@op.pl>
2023-09-29 19:53:33 +0200

    Refactor typedefs in shared lib

--- clm_libc/clm_cshared/clm_shared.h
+++ clm_libc/clm_cshared/clm_shared.h
@@ -9,27 +9,35 @@
  * TYPE ALIASES
  *****************************************************************************/
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+#ifndef CLM_TYPEDEFS
+#define CLM_TYPEDEFS(CLM_TYPEDEF)       \
+        CLM_TYPEDEF(uint8_t, u8)        \
+        CLM_TYPEDEF(uint16_t, u16)      \
+        CLM_TYPEDEF(uint32_t, u32)      \
+        CLM_TYPEDEF(uint64_t, u64)      \
+        CLM_TYPEDEF(int8_t, i8)         \
+        CLM_TYPEDEF(int16_t, i16)       \
+        CLM_TYPEDEF(int32_t, i32)       \
+        CLM_TYPEDEF(int64_t, i64)       \
+        CLM_TYPEDEF(uint8_t, byte)      \
+        CLM_TYPEDEF(uintptr_t, usize)   \
+        CLM_TYPEDEF(intptr_t, isize)    \
+        CLM_TYPEDEF(uintptr_t, uptr)    \
+        CLM_TYPEDEF(intptr_t, iptr)     \
+        CLM_TYPEDEF(ptrdiff_t, ptrdiff) \
+        CLM_TYPEDEF(float, f32)         \
+        CLM_TYPEDEF(double, f64)        \
+        CLM_TYPEDEF(long double, f128)
+#endif /* CLM_TYPEDEFS */
 
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+#ifndef CLM_TYPEDEF
+#define CLM_TYPEDEF(ty, alias) typedef ty alias;
 
-typedef u8 byte;
+#endif /* CLM_TYPEDEF */
 
-typedef uintptr_t usize;
-typedef intptr_t isize;
+CLM_TYPEDEFS(CLM_TYPEDEF)
 
-typedef uintptr_t uptr;
-typedef intptr_t iptr;
-typedef ptrdiff_t ptrdiff;
-
-typedef float f32;
-typedef double f64;
-typedef long double f128;
+#undef CLM_TYPEDEFS
+#undef CLM_TYPEDEF
 
 #endif /* CLM_SHARED_GUARD_ */
