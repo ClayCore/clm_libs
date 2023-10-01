2af2c114ea89d1d5a1a0a9aec45aa294861b5a91
lunarium <claymorealt@op.pl>
2023-10-01 15:47:16 +0200

    Add IPO/LTO support

---
+++ cmake/InterproceduralOptimization.cmake
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------------------------------------------------- #
+# `InterproceduralOptimization.cmake`
+# Check and enable IPO
+# ----------------------------------------------------------------------------------------------------- #
+
+macro(clm_enable_ipo)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT result OUTPUT output)
+
+    if(result)
+        set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+    else()
+        message(SEND_ERROR "IPO is not supported: ${output}")
+    endif()
+endmacro()
