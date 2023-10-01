ac5fef8b5f1333e9be9d370865b3d4af10308f9d
lunarium <claymorealt@op.pl>
2023-10-01 15:46:40 +0200

    Add fuzzing support using `libfuzzer`

---
+++ cmake/LibFuzzer.cmake
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------------------------------- #
+# `LibFuzzer.cmake`
+# Check for LLVM LibFuzzer support
+# ----------------------------------------------------------------------------------------------------- #
+
+function(clm_check_libfuzzer_support var_name)
+    set(
+        LibFuzzerTestSource
+        "
+            #include <cstdint>
+
+            extern \"C\" int LLVMFuzzerTestOneInput(const std::uint8_t *data, std::size_t size) {
+                return 0;
+            }
+        "
+    )
+
+    include(CheckCXXSourceCompiles)
+
+    set(CMAKE_REQUIRED_FLAGS "-fsanitize=fuzzer")
+    set(CMAKE_REQUIRED_LINK_OPTIONS "-fsanitize=fuzzer")
+
+    check_cxx_source_compiles("${LibFuzzerTestSource}" ${var_name})
+endfunction()
