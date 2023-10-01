1275cd8c2a82106f0e47b0d580610f22e55bfcab
lunarium <claymorealt@op.pl>
2023-10-01 15:47:22 +0200

    Add sane project defaults

---
+++ cmake/StandardProjectSettings.cmake
@@ -0,0 +1,51 @@
+# ----------------------------------------------------------------------------------------------------- #
+# `StandardProjectSettings.cmake`
+# Defines standard settings for our CMake project
+# ----------------------------------------------------------------------------------------------------- #
+
+# Sets default build type if none was specified
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "** Setting build type to 'RelWithDebInfo' as none was specified.")
+    set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "Choose the type of build" FORCE)
+
+    # Set the properties for cmake-gui/ccmake
+    set_property(
+        CACHE
+        CMAKE_BUILD_TYPE
+        PROPERTY
+        STRINGS
+        "Debug"
+        "Release"
+        "MinSizeRel"
+        "RelWithDebInfo"
+    )
+endif()
+
+# Generate `compile_commands.json` for clang based tools
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Enhance error reporting and compiler messages
+if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    if(WIN32)
+        add_compile_options($<$<COMPILE_LANGUAGE:C>:-fcolor-diagnostics> $<$<COMPILE_LANGUAGE:CXX>:-fcolor-diagnostics>)
+    else()
+        add_compile_options(-fcolor-diagnostics)
+    endif()
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    if(WIN32)
+        add_compile_options(
+            $<$<COMPILE_LANGUAGE:C>:-fdiagnostics-color=always>
+            $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>
+        )
+    else()
+        add_compile_options(-fdiagnostics-color=always)
+    endif()
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND MSVC_VERSION GREATER 1900)
+    add_compile_options(/diagnostics:column)
+else()
+    message(STATUS "** No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
+endif()
+
+# Run `vcvarsall` when msvc is used
+include("${CMAKE_CURRENT_LIST_DIR}/VCEnvironment.cmake")
+run_vcvarsall()
