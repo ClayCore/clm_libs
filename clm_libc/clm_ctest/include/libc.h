f6d555347b0ae5847c1d8b844b4300616b4c1662
lunarium <claymorealt@op.pl>
2023-10-02 22:37:27 +0200

    Remove old pch includes

--- clm_libc/clm_ctest/include/libc.h
+++
@@ -1,9 +0,0 @@
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
