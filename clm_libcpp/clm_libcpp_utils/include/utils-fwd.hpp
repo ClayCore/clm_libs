b0c53865c188ed975f64c397d551bb45f3af094b
lunarium <claymorealt@op.pl>
2023-10-28 19:17:31 +0200

    Add stub for forward-declare utils header

---
+++ clm_libcpp/clm_libcpp_utils/include/utils-fwd.hpp
@@ -0,0 +1,10 @@
+#ifndef CLM_LIBCPP_UTILS_FWD_GUARD
+#define CLM_LIBCPP_UTILS_FWD_GUARD
+
+#include "clm_libcpp_shared.hpp"
+
+namespace clm::utils::meta
+{
+}  // namespace clm::utils::meta
+
+#endif /* CLM_LIBCPP_UTILS_FWD_GUARD */
