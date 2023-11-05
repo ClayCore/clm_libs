341e64ca33cfa98ca8cb857c50b6b620be611a18
lunarium <claymorealt@op.pl>
2023-11-05 13:22:28 +0100

    Reformat traits module and add `Allocator` trait

--- clm_libcpp/clm_libcpp_traits/include/traits.hpp
+++ clm_libcpp/clm_libcpp_traits/include/traits.hpp
@@ -11,7 +11,53 @@
 
 namespace clm::traits
 {
-    template <typename Derived>
+    /*****************************************************************************************
+     * NonCopyable interface
+     *
+     * Derive to ensure a type cannot be copied or copy-assigned
+     ****************************************************************************************/
+    class INonCopyable
+    {
+    public:
+        constexpr INonCopyable() = default;
+        virtual ~INonCopyable()  = default;
+
+        INonCopyable(INonCopyable const &)                     = delete;
+        auto operator=(INonCopyable const &) -> INonCopyable & = delete;
+    };
+
+    /*****************************************************************************************
+     * NonMovable interface
+     *
+     * Derive to ensure a type cannot be moved or move-assigned
+     ****************************************************************************************/
+    class INonMovable
+    {
+    public:
+        constexpr INonMovable() = default;
+        virtual ~INonMovable()  = default;
+
+        INonMovable(INonMovable &&)                     = delete;
+        auto operator=(INonMovable &&) -> INonMovable & = delete;
+    };
+
+    /*****************************************************************************************
+     * NonCopyMovable interface
+     *
+     * Derive to ensure a type cannot be moved, copied, copy-assigned or move-assigned
+     ****************************************************************************************/
+    class INonCopyMovable
+      : public INonCopyable
+      , public INonMovable
+    {
+    };
+
+    /*****************************************************************************************
+     * Display trait
+     *
+     * Used for types that are meant to be displayed or logged nicely.
+     ****************************************************************************************/
+    template <class Derived>
     class TDisplay
     {
     private:
@@ -44,6 +90,34 @@ namespace clm::traits
         {
             return os << display.to_string();
         }
+    };
+
+    /*****************************************************************************************
+     * Allocator trait
+     *
+     * Used as a common base type for memory allocators
+     ****************************************************************************************/
+    template <class Derived, class Block>
+    class TAllocator
+      : public TDisplay<TAllocator<Derived, Block>>
+      , public INonCopyMovable
+    {
+    private:
+        using base_type    = TAllocator<Derived, Block>;
+        using derived_type = Derived;
+
+        auto inline derived() -> derived_type &;
+        auto inline derived() const -> derived_type const &;
+
+    public:
+        using allocator_type = derived_type;
+        using block_type     = Block;
+
+        auto alloc(usize size) -> block_type;
+        auto owns(block_type const &block) const -> bool;
+        auto free(block_type &block) -> void;
+
+        auto to_string_impl(u32 indent = 0) const -> std::string;
     };
 }  // namespace clm::traits
 
--- clm_libcpp/clm_libcpp_traits/include/traits.tpp
+++ clm_libcpp/clm_libcpp_traits/include/traits.tpp
@@ -6,32 +6,77 @@
 
 namespace clm::traits
 {
-    template <typename Derived>
+    /* #region TDisplayImplementation */
+
+    template <class Derived>
     auto inline TDisplay<Derived>::derived() -> derived_type &
     {
         return static_cast<derived_type &>(*this);
     }
 
-    template <typename Derived>
+    template <class Derived>
     auto inline TDisplay<Derived>::derived() const -> derived_type const &
     {
         return static_cast<derived_type const &>(*this);
     }
 
-    template <typename Derived>
+    template <class Derived>
     auto TDisplay<Derived>::to_string(u32 indent) const -> std::string
     {
         return this->derived().to_string_impl(indent);
     }
 
-    template <typename Derived>
+    template <class Derived>
     auto TDisplay<Derived>::display(u32 indent) const -> void
     {
         std::cout << this->to_string(indent);
     }
+
+    /* #endregion TDisplayImplementation */
+
+    /* #region TAllocatorImplementation */
+
+    template <class D, class Block>
+    auto inline TAllocator<D, Block>::derived() -> derived_type &
+    {
+        return static_cast<derived_type &>(*this);
+    }
+
+    template <class D, class Block>
+    auto inline TAllocator<D, Block>::derived() const -> derived_type const &
+    {
+        return static_cast<derived_type const &>(*this);
+    }
+
+    template <class D, class Block>
+    auto TAllocator<D, Block>::alloc(usize size) -> block_type
+    {
+        return this->derived().alloc_impl(size);
+    }
+
+    template <class D, class Block>
+    auto TAllocator<D, Block>::owns(block_type const &block) const -> bool
+    {
+        return this->derived().owns_impl(block);
+    }
+
+    template <class D, class Block>
+    auto TAllocator<D, Block>::free(block_type &block) -> void
+    {
+        this->derived().free_impl(block);
+    }
+
+    template <class D, class Block>
+    auto TAllocator<D, Block>::to_string_impl(u32 indent) const -> std::string
+    {
+        return this->derived().to_string(indent);
+    }
+
+    /* #endregion TAllocatorImplementation */
+
 }  // namespace clm::traits
 
-template <clm::meta::IsDisplay T, typename CharT>
+template <clm::meta::IsDisplay T, class CharT>
 struct std::formatter<T, CharT>: public std::formatter<std::string>
 {
     template <class FmtCtx>
