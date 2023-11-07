7248f7942b0161c6fbcb63ca552088a1c35d45b7
lunarium <claymorealt@op.pl>
2023-11-07 10:41:49 +0100

    Add all allocator submodules to main header

--- clm_libcpp/clm_libcpp_alloc/include/alloc.hpp
+++ clm_libcpp/clm_libcpp_alloc/include/alloc.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
 #include "chunk.hpp"
+
+#include "allocators/buffer_ref.hpp"
+#include "allocators/utils.hpp"
+
+#include "allocators/affix_allocator.hpp"
+#include "allocators/bitmapped_block.hpp"
+#include "allocators/bucketizer.hpp"
+#include "allocators/fallback_allocator.hpp"
+#include "allocators/free_list.hpp"
+#include "allocators/null_allocator.hpp"
+#include "allocators/segregator.hpp"
+#include "allocators/stack_allocator.hpp"
